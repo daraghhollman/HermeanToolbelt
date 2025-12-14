db52652aa1f19128c3e5c7ab2545f40176800e4a
Daragh Hollman <hollmandaragh@gmail.com>
2025-12-14 16:53:30 +0000

    Add test to check MAG data location is correct

--- tests/utils/test_web.py
+++ tests/utils/test_web.py
@@ -10,6 +10,28 @@ class Test_Web_Access(unittest.TestCase):
         r = requests.head(Urls.PDS_BASE)
         self.assertEqual(r.status_code, 200)
 
+    def test_mag_location(self):
+        """
+        Checks if MESSENGER MAG data is still following the correct format
+        """
+        url = (
+            Urls.PDS_BASE
+            + Urls.MAG_EXTENSION
+            + "2011/091_120_APR/MAGMSOSCI11113_V08.TAB"
+        )
+        r = requests.head(url)
+
+        self.assertEqual(r.status_code, 200)
+
+        url = (
+            Urls.PDS_BASE
+            + Urls.MAG_EXTENSION_AVG
+            + "2011/091_120_APR/MAGMSOSCI11113_01_V08.TAB"
+        )
+        r = requests.head(url)
+
+        self.assertEqual(r.status_code, 200)
+
 
 if __name__ == "__main__":
     unittest.main()
